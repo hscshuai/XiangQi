02761ae66b1de9c7c193411cac333b2e437c7767
hansicong <hansc0625@outlook.com>
2024-08-29 23:15:36 +0800

    添加棋盘

---
+++ src/view/Chessboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="chessboard">
+    <div class="point" v-for="point,index in points" :key="index"></div>
+  </div>  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const points = ref(new Array(90))
+</script>
+
+<style scoped>
+.chessboard {
+  position: relative;
+  top: calc(50% - 450px);
+  height: 900px;
+  width: 900px;
+  margin: auto;
+  background: url(./../assets/img/chessboard.jpg) center/100% 100%;
+}
+.point {
+  height: 90px;
+  width: 100px;
+}
+</style>
