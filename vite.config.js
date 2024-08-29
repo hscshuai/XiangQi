d332219f6485a0b80304a7381c749556a9518cbf
hansicong <hansc0625@outlook.com>
2024-08-29 10:57:58 +0800

    初始代码

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8000,
+    host: true,
+    open: false,
+    proxy: {
+      '/dev-api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/dev-api/, '')
+      }
+    }
+  }
+})
